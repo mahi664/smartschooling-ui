4e3dbd910a11e6453d191b2dd4bc3466e6f2c95d
Mahesh Ghuge <mahighuge664@gmail.com>
2022-01-22 13:47:15 +0530

    Implement Filter Functionality on students list screen #3

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ClassesService } from '../services/classes.service';
 import { CommonService } from '../services/common.service';
 import { StudentService } from '../services/student.service';
 
@@ -37,14 +38,29 @@ export class StudentDetailsBO{
 export class StudentListComponent implements OnInit {
 
   studentsList : StudentDetailsBO[] = [];
+  studentsListOrg : StudentDetailsBO[] = [];
   currentAcademicId : string = "AY-2021-22";
+  isFilterExpanded = false;
+  filteredData = {};
+  classesList : ClassesDetailsBO[] = [];
   constructor(private studentService: StudentService, private router: Router, 
-    private commonService : CommonService) { }
+    private commonService : CommonService, private classesService : ClassesService) { }
 
   ngOnInit() {
+
+    this.classesService.getClassesNames().subscribe(
+      response=>{
+        this.classesList = response;
+        // console.log(this.classesList);
+      },
+      error=>{
+        console.log(error);
+      }
+    );
     console.log('getting students list...');
     this.studentService.getStudentsList().subscribe(
       response=>{
+        this.studentsListOrg = response;
         this.studentsList = response;
         this.studentService.studentList = this.studentsList
         console.log(this.studentsList);
@@ -66,20 +82,82 @@ export class StudentListComponent implements OnInit {
     this.router.navigate(['/student-details',studentId]);
   }
 
-  printStudentData(){
-    document.getElementById("sideNav").style.display="none";
-    document.getElementById("topNav").style.display="none";
-    document.getElementById("students-list").style.display="none";
-    document.getElementById("modal-header").style.display="none";
-    document.getElementById("modal-footer").style.display="none";
+  filterStudentsOnAddressChange(){
+    console.log("Filtering students on address change");
+    if(this.filteredData['address']===undefined || this.filteredData['address']===" "){
+      this.studentsList = this.studentsListOrg;
+    }
+    if(this.filteredData['address']!=undefined){
+      this.studentsList = this.studentsListOrg.filter(
+        student => student.address.toLowerCase().includes(this.filteredData['address'].toLowerCase())
+      );
+    }
+  }
 
-    window.print();
+  applyFilter(){
+    this.studentsList = this.studentsListOrg;
     
-    document.getElementById("sideNav").style.display="block";
-    document.getElementById("topNav").style.display="block";
-    document.getElementById("students-list").style.display="block";
-    document.getElementById("modal-header").style.display="block";
-    document.getElementById("modal-footer").style.display="inline-block";
+    if(this.filteredData['address']!=undefined && this.filteredData['address']!=" "){
+      this.studentsList = this.studentsList.filter(
+        student => student.address.toLowerCase().includes(this.filteredData['address'].toLowerCase())
+      );
+    }
+    
+    if(this.filteredData['classId']!=undefined && this.filteredData['classId']!=" "){
+      this.studentsList = this.studentsList.filter(
+        student=>student.studentClassDetails[this.currentAcademicId][0].classId===this.filteredData['classId']
+      );
+    }
+
+    if(this.filteredData['transportOpted']!=undefined && this.filteredData['transportOpted']!=" "){
+      let transportOpted = this.filteredData['transportOpted'] === "YES" ? true : false;
+      this.studentsList = this.studentsList.filter(
+        student=>student.transportOpted === transportOpted
+      );
+    }
+
+    if(this.filteredData['gender']!=undefined && this.filteredData['gender']!=" "){
+      this.studentsList = this.studentsList.filter(
+        student=>student.gender.toLowerCase() === this.filteredData['gender'].toLowerCase()
+      );
+    }
+
+    if(this.filteredData['caste']!=undefined && this.filteredData['caste']!=" "){
+      this.studentsList = this.studentsList.filter(
+        student=>student.caste.toLowerCase() === this.filteredData['caste'].toLowerCase()
+      );
+    }
+  }
+
+  clearFilter(){
+    this.filteredData = {};
+    this.studentsList = this.studentsListOrg;
+  }
+
+  printStudentData(){
+    // document.getElementById("sideNav").style.visibility="hidden";
+    // document.getElementById("topNav").style.visibility="hidden";
+    // document.getElementById("students-list").style.visibility="hidden";
+    // document.getElementById("modal-header").style.visibility="hidden";
+    // document.getElementById("modal-footer").style.visibility="hidden";
+    
+    // document.getElementById("model-body").classList.remove();
+    
+    // window.print();
+    
+    // document.getElementById("sideNav").style.visibility="visible";
+    // document.getElementById("topNav").style.visibility="visible";
+    // document.getElementById("students-list").style.visibility="visible";
+    // document.getElementById("modal-header").style.visibility="visible";
+    // document.getElementById("modal-footer").style.visibility="visible";
+
+    const printContent = document.getElementById("printContent");
+    const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+    WindowPrt.document.write(printContent.innerHTML);
+    WindowPrt.document.close();
+    WindowPrt.focus();
+    WindowPrt.print();
+    WindowPrt.close();
   }
 
   // dateFormat(date : Date){
