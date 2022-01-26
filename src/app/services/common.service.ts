6b60033cf890497751a81061b0e7574555645b9c
Mahesh Ghuge <mahighuge664@gmail.com>
2022-01-26 22:52:44 +0530

    Commit for deployment

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -7,8 +7,8 @@ export class CommonService {
 
   sideNavExpanded = true;
   isLoading = false;
-  // public BASE_URL = "http://localhost:8080/demo-0.0.1-SNAPSHOT";
-  public BASE_URL = "http://localhost:8080";
+  public BASE_URL = "https://smartschooling.herokuapp.com/";
+  //public BASE_URL = "http://localhost:8080";
   // public BASE_URL = "http://demo-env.eba-sdsq2q4z.ap-south-1.elasticbeanstalk.com";
   constructor() { }
 
