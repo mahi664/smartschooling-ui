adf852afe25dc2c14f80b28bed68f0b2f7894dda
Mahesh Ghuge <mahighuge664@gmail.com>
2022-01-27 22:30:01 +0530

    Create an UI to add new route details #32 , Create a UI to display all the Routes configured in the system #31

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { ClassesComponent } from './classes/classes.component';
 import { FeeDetailsComponent } from './fee-details/fee-details.component';
 import { FeeTypesComponent } from './fee-types/fee-types.component';
 import { NewFeeTypeComponent } from './new-fee-type/new-fee-type.component';
+import { RoutesComponent } from './routes/routes.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { StudentListComponent } from './student-list/student-list.component';
 
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: "fee-types", component: FeeTypesComponent},
   {path: "fee-types/new-type/:feeType", component: NewFeeTypeComponent},
   {path: "fee-details/:feeType", component: FeeDetailsComponent},
-  {path: "classes", component: ClassesComponent}
+  {path: "classes", component: ClassesComponent},
+  {path: "routes", component: RoutesComponent}
 ];
 
 @NgModule({
