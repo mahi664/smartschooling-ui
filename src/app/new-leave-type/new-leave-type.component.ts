16f35cf32844e3a2bc31f4ed89531a972fdfa127
Mahesh Ghuge <mahighuge664@gmail.com>
2022-04-16 17:05:24 +0530

    Build failed issue resolved..

--- src/app/new-leave-type/new-leave-type.component.ts
+++ src/app/new-leave-type/new-leave-type.component.ts
@@ -64,7 +64,7 @@ export class NewLeaveTypeComponent implements OnInit {
     this.validationFlags['amount'] = false;
   }
 
-  validateField(field : string, value : string){
+  validateField(field : string, value : any){
     this.validationFlags[field]= value==="" ? true : false;
     if(field==="accrualFreq"){
       value!="Daily" ? this.isAccrualDayApplicable = true : this.isAccrualDayApplicable = false;
